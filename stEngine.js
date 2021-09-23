d5cacd4add3bd1aa626356423a29fe36df507e2c
Sta-ces <staescedric@gmail.com>
2021-09-23 21:59:16 +0200

    modification method sound of stEngine (name + return variable)

--- stEngine.js
+++ stEngine.js
@@ -24,10 +24,11 @@ export default class stEngine{
         requestAnimationFrame(() => this.#update());
     }
 
-    PlaySound({src, volume = 1}){
+    sound({src, volume = 1}){
         let sound = new Audio();
         sound.src = src;
         sound.volume = (volume > 1) ? 1 : (volume < 0) ? 0 : volume;
         sound.play();
+        return sound;
     }
 }
