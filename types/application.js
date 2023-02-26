4b26f60083fbf17d1b9aa418f9d22f1c09bd82e4
Sta-ces <staescedric@gmail.com>
2023-02-26 19:55:53 +0100

    Update class 'Application'

--- types/application.js
+++ types/application.js
@@ -22,23 +22,24 @@ export default class Application extends System{
     screen(nameScreen){
         const screen = document.querySelector(`.screen-element[name='${nameScreen}']`)
         if(screen) screen.setAttribute("state", "open")
+        return screen
     }
 
     create({element = "div", classname = "", id = "", src = "", alt = "", data = "", type = "", attr = {}, parent = null, textContent = null, append = true}){
-        const el = document.createElement(element);
-        if(classname !== "") el.setAttribute("class", classname);
-        if(id !== "") el.setAttribute("id", id);
-        if(src !== "") el.setAttribute("src", src);
-        if(data !== "") el.setAttribute("data", data);
-        if(type !== "") el.setAttribute("type", type);
-        if(alt !== "") el.setAttribute("alt", alt);
+        const el = document.createElement(element)
+        if(classname !== "") el.setAttribute("class", classname)
+        if(id !== "") el.setAttribute("id", id)
+        if(src !== "") el.setAttribute("src", src)
+        if(data !== "") el.setAttribute("data", data)
+        if(type !== "") el.setAttribute("type", type)
+        if(alt !== "") el.setAttribute("alt", alt)
         if(Object.keys(attr).length > 0){
             for(const [key, value] of Object.entries(attr))
                 el.setAttribute(key, value)
         }
-        if(textContent) el.innerHTML = textContent;
-        if(parent) parent.appendChild(el);
-        else if(append) this.append(el);
-        return el;
+        if(textContent) el.innerHTML = textContent
+        if(parent) parent.appendChild(el)
+        else if(append) this.append(el)
+        return el
     }
 }
